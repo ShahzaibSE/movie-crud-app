0cdcfa7c176cfa7a5abd86c28127aba9212da2a2
ShahzaibSE <shahzaibanalyst@outlook.com>
2017-08-05 19:19:21 +0500

    Movie list component in progress

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,22 +11,22 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 
-//Service
+// Service
 import { ApiServiceService } from './api-service.service';
 import { EditMovieComponent } from './edit-movie/edit-movie.component';
 import { AuthService } from './auth.service';
 import { LoggedInGuard } from './guard.service';
 
-//Routes
+// Routes
 import { routes } from './app.routes';
 import { LoginComponent } from './login/login.component';
 
-//ReCaptcha
+// ReCaptcha
 import { RecaptchaModule } from 'ng2-recaptcha';
 import { ReCaptchaComponent } from './re-captcha/re-captcha.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-//Material Imports
+// Material Imports
 import { MdCardModule } from '@angular/material';
 import { SampleProtectedRouteComponent } from './sample-protected-route/sample-protected-route.component';
 
--- client/src/app/movie-list/movie-list.component.ts
+++ client/src/app/movie-list/movie-list.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  dummy_movie_data = [
+    { mid : 1, title : 'Batman vs Superman : Dawn of Justice', director : 'Zack Snyder', producer : 'Christopher Nolan'  },
+    { mid : 2, title : 'Resident Evil: Vendetta', director : 'Zack Snyder', producer : 'Christopher Nolan'  },
+    { mid : 3, title : 'John Wick : Chapter1', director : 'Zack Snyder', producer : 'Christopher Nolan'  },
+    { mid : 4, title : 'John Wick : Chapter2', director : 'Zack Snyder', producer : 'Christopher Nolan'  },
+    { mid : 5, title : 'Justice League', director : 'Zack Snyder', producer : 'Christopher Nolan'  }
+  ];
+
   constructor() { }
 
   ngOnInit() {
